a379b10ade90d5f075644331e92ded31f674f51e
SSIS students <43933271+kreier@users.noreply.github.com>
2023-02-24 09:53:01 +0700

    update

--- circuitpython/apps/Prime1000.py
+++ circuitpython/apps/Prime1000.py
@@ -1,21 +1,20 @@
 import math, time
-last = 1000
-found = 4          # we start from 11, know 2, 3, 5, 7
-print(f"Primes to {last}")
-print('2, 3, 5, 7',end='')
-start = time.monotonic()
-for number in range(11, last, 2):
-    prime = True
-    for divider in range(3, int(math.sqrt(number))+1, 2):
-        if number % divider == 0:
-            prime = False
-            break
-    if prime:
-        print(",", number, end='')
-        found += 1
-        prime = 1
-end = time.monotonic()
-print(f"\nIt took: {(end - start)} s.")
-print(f"Found {found} numbers.")
 
-time.sleep(5)
+last = 1000
+
+start = time.monotonic()
+print('Prime numbers to {}'.format(last))
+
+#print('2, 3, 5, 7',end='')
+for number in range(11, last, 2):
+    prime = 1
+    for divider in range(2, int(math.sqrt(number))+1, 1):
+        if number/divider == int(number/divider):
+            prime = 0
+
+    if prime == 1:
+        #print(',', number, end='')
+        prime = 1
+
+end = time.monotonic()
+print('\nThis took:', (end - start), 'seconds.')
