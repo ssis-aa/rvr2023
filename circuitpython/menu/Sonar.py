ae1c6765f1aac384a435493ee6ec9d67c5cb13b9
SSIS students <43933271+kreier@users.noreply.github.com>
2023-03-01 11:09:33 +0700

    sonar with distance every 200 ms

---
+++ circuitpython/menu/Sonar.py
@@ -0,0 +1,16 @@
+# This is my sonar program
+# Trigger pin: GP4
+# Echo Pin:    GP5
+
+import time
+import board
+import adafruit_hcsr04
+
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP4, echo_pin=board.GP5)
+
+while True:
+    try:
+        print("(",sonar.distance,")")
+    except RuntimeError:
+        print("Retrying!")
+    time.sleep(0.2)
