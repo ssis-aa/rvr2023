f167e95788e1f55185d1f7aa6150e22f8c5b5db6
SSIS students <43933271+kreier@users.noreply.github.com>
2023-03-03 08:23:53 +0700

    adjust pins to GP6 and GP7

--- circuitpython/menu/Sonar.py
+++ circuitpython/menu/Sonar.py
@@ -6,7 +6,7 @@ import time
 import board
 import adafruit_hcsr04
 
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP4, echo_pin=board.GP5)
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP6, echo_pin=board.GP7)
 
 while True:
     try:
