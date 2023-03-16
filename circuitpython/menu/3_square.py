358057238bb852b1829070499c32728ecc1be9bb
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-03-16 09:10:22 +0700

    create artifact of square assignment

---
+++ circuitpython/menu/3_square.py
@@ -0,0 +1,5 @@
+# Robot will first drive a 60cm square
+# It then use the ultrasonic sensor to keep a 25cm distance from any object in front of it
+
+# Mu crashed and the code is lost
+# It worked on March 15th, have to reload it
